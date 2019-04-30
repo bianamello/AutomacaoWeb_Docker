6bb348bd600cb6846933c4d98003b39ea29640e4
Fabiana <fabiana.mello@dbserver.com.br>
2019-04-30 14:00:48 -0300

    Criando setup do projeto

---
+++ features/support/env.rb
@@ -0,0 +1,33 @@
+require 'capybara'
+require 'capybara/cucumber'
+require 'faker'
+require 'rspec'
+require 'selenium-webdriver'
+require 'site_prism'
+
+@browser = ENV['BROWSER']
+
+if @browser.eql?('headless')
+    Capybara.javascript_driver = :selenium
+    Capybara.run_server = false
+
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+        'chromeOptions' => {'args' => ['--no-default-browser-check']}
+    )
+
+    Capybara.register_driver :selenium_chrome do |app|
+        Capybara::Selenium::Driver.new(
+            app,
+            browser: :remote,
+            url: 'http://selenium:4444/wd/hub',
+            desired_capabilities: caps
+        )
+    end
+else
+    puts 'executando sem headless'
+end
+
+Capybara.configure do |config|
+    config.default_driver = :selenium_chrome
+    config.default_max_wait_time = 30
+end
